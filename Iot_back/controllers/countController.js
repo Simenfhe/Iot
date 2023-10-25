6bc048753ffd974ea3013ec2f645c1cf91f39f02
BuildTools <simenfhe@stud.ntnu.no>
2023-10-25 22:00:33 +0200

    startscript

--- Iot_back/controllers/countController.js
+++ Iot_back/controllers/countController.js
@@ -1,11 +1,23 @@
 
 //get count
 const getCount = async (req, res) => {
-  console.log('hallo')
-    const showPlayers = "moradi"
-    res.send(showPlayers);
+  console.log("teller")
+    const amount = "3"
+    res.send(amount);
 };
 
+const getTemp = async (req,res) => {
+  console.log("temp")
+  const temp = "22"
+    res.send(temp);
+}
+
+const getAir = async (req,res) => {
+  console.log("Air")
+  const air = "84"
+    res.send(air);
+}
+
 module.exports = {
-  getCount
+  getCount, getTemp, getAir
 };
--- Iot_back/routers/countRoutes.js
+++ Iot_back/routers/countRoutes.js
@@ -2,11 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  getCount,
+  getCount, getTemp, getAir
 } = require("../controllers/countController");
 
 //ALL count 
 router.get("/count", getCount);
+
+router.get("/temp", getTemp);
+
+router.get("/air", getAir);
+
 
 
 
