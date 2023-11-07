7f6612b44d0e125c0c9d7cc32a6651835fae4c7c
BuildTools <simenfhe@stud.ntnu.no>
2023-11-07 19:35:42 +0100

    components

--- Iot_front/Routes/Landing.jsx
+++ Iot_front/Routes/Landing.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from '../functions/axios';
 import Meter from '../components/Meter/Meter';
+import Map from '../components/Map/Map';
 
 // import sensordata
 
@@ -12,18 +13,17 @@ function LandingPage() {
     useEffect(() => {
         axios.get("/counter").then((response) => {
             setCount(response.data)
-            console.log(response.data)
         })
 
-        axios.get("/temp").then((response) => {
-            setTemp(response.data)
-            console.log(temp)
-        })
+        // axios.get("/temp").then((response) => {
+        //     setTemp(response.data)
+        //     console.log(temp)
+        // })
 
-        axios.get("/air").then((response) => {
-            setAir(response.data)
-            console.log(air)
-        })
+        // axios.get("/air").then((response) => {
+        //     setAir(response.data)
+        //     console.log(air)
+        // })
 
     }, [])
 
@@ -31,17 +31,13 @@ function LandingPage() {
     return (
         <main>
             <h1>Vælkømmin til gaars</h1>
-            <Meter percentage="-32" />
+            <Meter percentage="90" />
             <ul>
                 <li>Det er {count}stk i rommet</li>
                 <li>Det er {temp} grader der</li>
                 <li>Det er {air} luft et eller annet</li>
             </ul>
-            <svg width="715" height="500" viewBox="0 0 715 500" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path class="mapelement room" onclick="console.log('1')" d="M58.5 110.5L217 46.9999L340.5 0.5L383 21L422 115.5L142.5 226.996L104 126.5L70.5 140L58.5 110.5Z" fill="currentColor" />
-                <path class="mapelement room" onclick="console.log('2')" d="M322 163.5L429.5 122L433 125.5L551 79L629.5 47.5L715 247.504L435.5 359L312.5 404L305 389L252 408.5L201 285.5L345.5 226.5L322 163.5Z" fill="currentColor" />
-                <path class="mapelement hallway" onclick="console.log('3')" d="M163 233.5L274.5 187L272 175.5L299.5 165L302.5 173L322.5 166.5L344 221.5L195 282L268.5 474.5L208 497.5L133.5 307L131 299.5L127 301L102 242L96.5 244.5L89 227.5L86 220L35 241L0.5 160.5L55.5 137.5L62 154.5L75 149.5L93.5 142L128 225L122 227.5L129 246L151.5 237.5L157.5 250.5L168 247L163 233.5Z" fill="currentColor" />
-            </svg>
+            <Map />
 
         </main>
     )
