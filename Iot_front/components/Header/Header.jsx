e3c50990cd7a9607b513f16a92184b8ba98eea5f
simenfhe <simenfhe@stud.ntnu.no>
2023-11-15 11:01:05 +0100

    comeet

--- Iot_front/components/Header/Header.jsx
+++ Iot_front/components/Header/Header.jsx
@@ -3,14 +3,18 @@ import React from "react";
 
 function Header() {
     return (
-        <nav>
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Rooms</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Mora til håvard</a></li>
-            </ul>
-        </nav>
+        <>
+            <nav>
+                <img id="logo" src="../../public/Logo.svg" alt="" />
+
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Rooms</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Mora til håvard</a></li>
+                </ul>
+            </nav>
+        </>
     )
 }
 
