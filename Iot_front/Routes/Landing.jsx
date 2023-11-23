6e40e371762d4e0a114254bbcdf2517d6a0d8a0a
simenfhe <simenfhe@stud.ntnu.no>
2023-11-23 14:43:57 +0100

    histogram frontend componenet

--- Iot_front/Routes/Landing.jsx
+++ Iot_front/Routes/Landing.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from '../functions/axios';
 import Meter from '../components/Meter/Meter';
 import Map from '../components/Map/Map';
+import Histogram from '../components/Histogram/Histogram';
 
 // import sensordata
 
@@ -9,6 +10,8 @@ function LandingPage() {
     const [count, setCount] = useState("0")
     const [temp, setTemp] = useState("0")
     const [air, setAir] = useState("0")
+    const histogram = [0, 0, 3, 5, 12, 2, 4, 7, 12, 24, 25, 27, 21, 22, 23, 25, 14, 11, 7, 6, 5, 0, 3, 1]
+    const capacity = 45
 
     useEffect(() => {
         // axios.get("/sse").then((response) => {
@@ -50,6 +53,7 @@ function LandingPage() {
                 <li>Det er {air} ledige plasser</li>
             </ul>
             <Map />
+            <Histogram histogram={histogram} capacity={capacity} />
 
         </main>
     )
